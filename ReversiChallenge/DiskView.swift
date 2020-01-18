11ef678bf7b0cfafb785d5dd4fba40fc1875c8bf
Yuta Koshizawa <koher@koherent.org>
2020-01-19 00:03:49 +0900

    Separate `DiskView` from CellView.swift

---
+++ ReversiChallenge/DiskView.swift
@@ -0,0 +1,64 @@
+import UIKit
+
+class DiskView: UIView {
+    var disk: Disk = .dark {
+        didSet { setNeedsDisplay() }
+    }
+    
+    @IBInspectable var name: String {
+        get { disk.name }
+        set { disk = .init(name: newValue) }
+    }
+    
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+    }
+    
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUp()
+    }
+
+    private func setUp() {
+        backgroundColor = .clear
+        isUserInteractionEnabled = false
+    }
+
+    override func draw(_ rect: CGRect) {
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        context.setFillColor(disk.cgColor)
+        context.fillEllipse(in: bounds)
+    }
+}
+
+extension Disk {
+    fileprivate var uiColor: UIColor {
+        switch self {
+        case .dark: return UIColor(named: "DarkColor")!
+        case .light: return UIColor(named: "LightColor")!
+        }
+    }
+    
+    fileprivate var cgColor: CGColor {
+        uiColor.cgColor
+    }
+    
+    fileprivate var name: String {
+        switch self {
+        case .dark: return "dark"
+        case .light: return "light"
+        }
+    }
+    
+    init(name: String) {
+        switch name {
+        case Disk.dark.name:
+            self = .dark
+        case Disk.light.name:
+            self = .light
+        default:
+            preconditionFailure("Illegal name: \(name)")
+        }
+    }
+}
