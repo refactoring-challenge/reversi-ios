ef00d31d563ba186e4c82d15edb9dc8f98935ea4
Kuniwak <orga.chem.job@gmail.com>
2020-05-06 11:54:51 +0900

    add: New project as ReversiView

---
+++ ReversiView/ReversiView/ReversiView.h
@@ -0,0 +1,19 @@
+//
+//  ReversiView.h
+//  ReversiView
+//
+//  Created by Yuki Kokubun on 2020/05/06.
+//  Copyright © 2020 Kuniwak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReversiView.
+FOUNDATION_EXPORT double ReversiViewVersionNumber;
+
+//! Project version string for ReversiView.
+FOUNDATION_EXPORT const unsigned char ReversiViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReversiView/PublicHeader.h>
+
+
